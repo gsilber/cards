c06d2e789e1c368936ed4c152cd3eb7f1df927a1
Greg Silber <silber@udel.edu>
2026-02-09 22:38:12 -0500

    cleanup

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,6 +8,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('cards');
+  protected readonly title = signal('VIP Card Trick');
 
 }
